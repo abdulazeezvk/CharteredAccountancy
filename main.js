fe82fdc80362ec22969894cfc65926b95b35b726
abdulazeez <abdulazeez.aa258@gmail.com>
2023-09-04 15:08:42 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+
+var icon = document.getElementById("mobilemenu");
+var hiddenBox = document.getElementById("hidden-div");
+
+icon.addEventListener("click", () =>{
+    
+  hiddenBox.classList.toggle("hidden");
+  
+});
